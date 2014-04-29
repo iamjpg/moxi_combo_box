3fa0b625084c378e08f0bc70da04abfc261accae
JP Given <jgiven@gmail.com>
2014-04-29 13:20:32 -0700

    Getting the window resizing stubbed out.

--- lib/moxi.combobox.coffee
+++ lib/moxi.combobox.coffee
@@ -3,6 +3,8 @@
 
   Plugin = (element, options) ->
     @element = element
+    @el = $(element)
+    @resizeListener = undefined
     @options = $.extend({}, defaults, options)
     @_defaults = defaults
     @_name = pluginName
@@ -17,7 +19,28 @@
   Plugin:: =
 
     init: ->
-      alert 1
+      @setResizeListener()
+      @setElementPosition()
+
+    setElementPosition: ->
+      # set the x/y of the element.
+      @el_pos_y = @el.offset().top
+      @el_pos_x = @el.offset().top
+
+    setResizeListener: ->
+      window.onresize = =>
+        clearTimeout @resizeListener
+        @resizeListener = setTimeout(=>
+          @resizedWindow()
+        , 200)
+        return
+
+    resizedWindow: ->
+      console.log "window resized"
+
+
+
+
 
 
   $.fn[pluginName] = (options) ->
--- src/moxi.combobox.js
+++ src/moxi.combobox.js
@@ -2,6 +2,8 @@
   var Plugin, defaults, pluginName;
   Plugin = function(element, options) {
     this.element = element;
+    this.el = $(element);
+    this.resizeListener = void 0;
     this.options = $.extend({}, defaults, options);
     this._defaults = defaults;
     this._name = pluginName;
@@ -13,7 +15,25 @@
   };
   Plugin.prototype = {
     init: function() {
-      return alert(1);
+      this.setResizeListener();
+      return this.setElementPosition();
+    },
+    setElementPosition: function() {
+      this.el_pos_y = this.el.offset().top;
+      return this.el_pos_x = this.el.offset().top;
+    },
+    setResizeListener: function() {
+      return window.onresize = (function(_this) {
+        return function() {
+          clearTimeout(_this.resizeListener);
+          _this.resizeListener = setTimeout(function() {
+            return _this.resizedWindow();
+          }, 200);
+        };
+      })(this);
+    },
+    resizedWindow: function() {
+      return console.log("window resized");
     }
   };
   $.fn[pluginName] = function(options) {
